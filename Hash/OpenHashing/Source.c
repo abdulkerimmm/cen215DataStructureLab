2bf3a1a46cc6a859d96df9b8b56a771a0747a0e3
Abbas Elmas <abbaselmas@gmail.com>
2019-12-05 16:49:19 +0300

    Revert "removed some functions"
    This reverts commit bcc267c412e65818f79dcc0abfa2f5bbda41afd7.

--- Hash/OpenHashing/Source.c
+++ Hash/OpenHashing/Source.c
@@ -10,15 +10,66 @@ typedef struct node
 node* a[max];
 void insert()
 {
-	
+	int d;
+	printf("Enter the element to be inserted : ");
+	scanf_s("%d", &d);
+	int x = d % max;
+	node* newnode = (node*)malloc(sizeof(node));
+	newnode->x = d;
+	newnode->next = NULL;
+	if (a[x] == NULL)
+		a[x] = newnode;
+	else
+	{
+		node *ptr;
+		ptr = a[x];
+		int flag = 1;
+		while (ptr->next != NULL)
+		{
+			if (ptr->x == d)
+				flag = 0;
+			ptr = ptr->next;
+		}
+		if (ptr->x == d)
+			flag = 0;
+		if (flag == 1)
+			ptr->next = newnode;
+		else
+			printf("Element already exists !");
+	}
 }
 void search()
 {
-	
+	printf("Enter the element you want to search :");
+	int d;
+	scanf_s("%d", &d);
+	int x = d % max;
+	node* ptr = a[x];
+	int flag = 0;
+	while (ptr != NULL)
+	{
+		if (ptr->x == d)
+			flag = 1;
+		ptr = ptr->next;
+	}
+	if (flag == 1)
+		printf("Element present");
+	else
+		printf("Element not found");
 }
 void display()
 {
-	
+	for (int i = 0; i < max; i++)
+	{
+		node* ptr = a[i];
+		printf("Index %d - ", i);
+		while (ptr != NULL)
+		{
+			printf("%d, ", ptr->x);
+			ptr = ptr->next;
+		}
+		printf("\n");
+	}
 }
 void main()
 {
